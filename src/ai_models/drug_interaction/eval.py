bc45b050fd2fa4418c0bb0e05c78607e7295cd79
Jordi Garcia <jordi.garcia@lambdaloopers.com>
2022-04-02 23:43:45 +0200

    Add legend

--- src/ai_models/drug_interaction/eval.py
+++ src/ai_models/drug_interaction/eval.py
@@ -4,6 +4,7 @@ import deepchem as dc
 import numpy as np
 from tensorflow import keras
 from tensorflow_addons.optimizers import AdamW
+from src.ai_models.drug_interaction.model.assets.legend import get as get_legend
 
 featurizer = dc.feat.CircularFingerprint()
 
@@ -18,7 +19,8 @@ def invoke_model(drugs: np.array) -> [float]:
     x = np.transpose(drugs)
     x = x.reshape(1, 64, 64, 1)
     scores = model.predict(x) * 100
-    return scores.astype(np.float)[0][8]
+    legend = get_legend()
+    return [{"label": legend[i+1], "value":round(x, 3)} for i, x in enumerate(scores.astype(np.float)[0])]
 
 
 def run_inference(smile1, smile2):
