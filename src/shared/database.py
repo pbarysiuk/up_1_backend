5ca0606f009cd3bab37d66457ee1a5e10eda9ad1
habibfrancis <habib@prepaire.com>
2022-06-03 21:33:36 +0300

    Update database.py to get the connection string from secret manager

--- src/shared/database.py
+++ src/shared/database.py
@@ -14,10 +14,10 @@ class Database:
         self.client.close()
 
     def __getConnectionStringFromSecret(self):
-        secret_name = "MONGODB_CONNSTRING"
+        secret_name = "dev/prepaire/documentDB"
         region_name = "us-east-1"
         session = boto3.session.Session()
         client = session.client(service_name='secretsmanager', region_name=region_name)
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-        return get_secret_value_response['SecretString']
+        return get_secret_value_response['SecretString']['MONGODB_CONNSTRING']
         
