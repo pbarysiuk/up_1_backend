bb2458860a85cf56fccfd06a032fdeb5103094ca
nandaAnil <anil@prepaire.com>
2022-06-21 13:24:48 +0400

    modify pipeline for 3dplot

--- .github/scripts/build-lambda.sh
+++ .github/scripts/build-lambda.sh
@@ -3,6 +3,7 @@
 for file in $FILES;do
 
 module=`echo $file | awk -F '/' '{print $2}'`
+if [[ $module != "3dplot" ]];then
 func=`echo $file | awk -F '/' '{print $3}'`
 
 chmod 777 $file
@@ -15,6 +16,21 @@ aws s3 cp backend-$module-$func-$COMMITID.zip s3://prepaire-$ENV-lambda-artifact
 aws s3 cp backend-$module-$func-latest.zip s3://prepaire-$ENV-lambda-artifact-us/latest/
 
 aws lambda update-function-code --function-name prepaire-$ENV-backend-$module-$func --s3-bucket prepaire-$ENV-lambda-artifact-us --s3-key backend-$module-$func-$COMMITID.zip
+
+else
+
+chmod 777 $file
+
+zip -j backend-$module-$COMMITID.zip $file
+
+cp backend-$module-$COMMITID.zip backend-$module-latest.zip
+
+aws s3 cp backend-$module-$COMMITID.zip s3://prepaire-$ENV-lambda-artifact-us/
+aws s3 cp backend-$module-latest.zip s3://prepaire-$ENV-lambda-artifact-us/latest/
+
+aws lambda update-function-code --function-name prepaire-$ENV-backend-$module --s3-bucket prepaire-$ENV-lambda-artifact-us --s3-key backend-$module-$COMMITID.zip
+
+fi
 
 done
 
