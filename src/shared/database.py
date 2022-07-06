89b37677f8f463f6425207aa1a8b420f515d0b81
habibfrancis <habib@prepaire.com>
2022-07-06 15:51:50 +0300

    change ps to env

--- src/shared/database.py
+++ src/shared/database.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
 from os import environ
-from src.shared.lambdaHelper import LambdaHelper
+#from src.shared.lambdaHelper import LambdaHelper
 
 class Database:
     def __init__(self):
-        connectionString = LambdaHelper.getValueFromParameterStore(envKey= "PARAM_STORE_MONGODB", defaultEnvKey="MONGODB_CONNSTRING")
+        connectionString = environ.get('MONGODB_CONNSTRING')
+        #LambdaHelper.getValueFromParameterStore(envKey= "PARAM_STORE_MONGODB", defaultEnvKey="MONGODB_CONNSTRING")
         self.client = MongoClient(connectionString)
         self.db = self.client.drugbank
 
